b9e1b8b4ac62a72845e6bd2968ded8e080bdca98
Noah230285 <noahjharris@outlook.com>
2024-03-31 00:55:27 +1100

    Fixed for addressables

--- Editor/CustomEditor.cs
+++ Editor/CustomEditor.cs
@@ -6,36 +6,36 @@ using UnityEngine.Events;
 
 namespace _S.Editor.CustomEditors
 {
-    [CustomEditor(typeof(Object), true, isFallback = true)]
+    //[CustomEditor(typeof(Object), true, isFallback = true)]
     public class DefaultEditor : UnityEditor.Editor
     {
-        protected StyleSheet defaultStyles;
+//        protected StyleSheet defaultStyles;
 
-        public override VisualElement CreateInspectorGUI()
-        {
-            var container = new VisualElement();
+//        public override VisualElement CreateInspectorGUI()
+//        {
+//            var container = new VisualElement();
 
-            var iterator = serializedObject.GetIterator();
+//            var iterator = serializedObject.GetIterator();
 
-            defaultStyles = AssetDatabase.LoadAssetAtPath<StyleSheet>
-("Packages/com.gameinvader.utility/Assets/Editor/UIToolkit/Styles/Main.uss");
-            container.styleSheets.Add(defaultStyles);
+//            defaultStyles = AssetDatabase.LoadAssetAtPath<StyleSheet>
+//("Packages/com.gameinvader.utility/Assets/Editor/UIToolkit/Styles/Main.uss");
+//            container.styleSheets.Add(defaultStyles);
 
-            if (iterator.NextVisible(true))
-            {
-                do
-                {
-                    var propertyField = new PropertyField(iterator.Copy()) { name = "PropertyField:" + iterator.propertyPath };
-                    propertyField.Bind(serializedObject);
-                    if (iterator.propertyPath == "m_Script" && serializedObject.targetObject != null)
-                        propertyField.SetEnabled(value: false);
-                    container.Add(propertyField);
-                }
-                while (iterator.NextVisible(false));
-            }
-            serializedObject.ApplyModifiedProperties();
+//            if (iterator.NextVisible(true))
+//            {
+//                do
+//                {
+//                    var propertyField = new PropertyField(iterator.Copy()) { name = "PropertyField:" + iterator.propertyPath };
+//                    propertyField.Bind(serializedObject);
+//                    if (iterator.propertyPath == "m_Script" && serializedObject.targetObject != null)
+//                        propertyField.SetEnabled(value: false);
+//                    container.Add(propertyField);
+//                }
+//                while (iterator.NextVisible(false));
+//            }
+//            serializedObject.ApplyModifiedProperties();
 
-            return container;
-        }
+//            return container;
+        //}
     }
 }
---
+++ Runtime/Utility/Classes/FilepathManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FilepathManager : MonoBehaviour
+{
+    public static string initialPath => "Packages/com.gameinvader.utility";
+}
